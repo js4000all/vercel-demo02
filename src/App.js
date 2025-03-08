da0255111c3b5acba0f158ecaaecc47e194cfd28
js4000all <js4000all@gmail.com>
2025-03-08 12:47:37 +0900

    bugfix

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Counter from "./Counter";
 
 function App() {
   return (
-    <h1>カウンターアプリ</h1>
-    <Counter />
-    <Counter />
-    <Counter />
+    <div>
+      <h1>カウンターアプリ</h1>
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
   );
 }
 
